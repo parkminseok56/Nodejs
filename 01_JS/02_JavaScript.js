167da3c15d00060d502589c675d80ff78d0d41a4
parkminseok56 <133790403+parkminseok56@users.noreply.github.com>
2023-07-13 14:12:35 +0900

    2023-07-13 1교시

---
+++ 01_JS/02_JavaScript.js
@@ -0,0 +1,26 @@
+var a = "Hello World & Node.js";
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);   
+
+// Nodejs의 정의 (공식 홈페이지 설명)
+// Node.js는 크롬 v8 자바스크립트 엔진으로 빌드된 자바스크립트 런타임(실행기)입니다.
+
+// Q:노드는 서버가 아닌가요? 서버라는 말이 없네여.
+// A:  서버의 역할도 수행 할 수 있는 자바스크립트 런타임임.
+// 노드로 자바스크립트로 작성된 서버를 실행 할 수 있음.
+// 서버 실행을 위해 필요한 http/https/http2 모듈을 제공함.
+
+// 노드: 자바스크립트 런타임.
+// 런타임: 특정 언어로 만든 프로그램들을 실행 할  수 있게 해주는 가상 머신의 상태
+// 다른 런타임이로는 웹 브라우저( 크롬, 엣지, 사파리, 파이어폭스 등)가 있음.
+// 노드 이전에도 자바스크립트 런타임을 만들기 위한 많을 시도를 했지만, 엔진 속도 문제로 실패함.
+
+
+// MERN Stack 개발자
+// MongoDB, Express Server, React, Node의 앞 자만 딴, 풀스택 개발자를 가르키는 신조어
+// MERN이 등장하기 이 전에는 Angular가 React 자리에 들어가서 MEAN이었지만,
+// 사용 선호도가 Angular보다 React가 높아지면서 변형됨.
