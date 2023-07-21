8aa8d5f1404b92f335e60e0aa3745cae873e8720
parkminseok56 <133790403+parkminseok56@users.noreply.github.com>
2023-07-21 16:10:43 +0900

    2023-07-21 6교시

--- Server12/models/comment.js
+++ Server12/models/comment.js
@@ -1,4 +1,3 @@
-
 const Sequelize = require('sequelize')
 
 module.exports = class Comment extends Sequelize.Model {
@@ -8,7 +7,7 @@ module.exports = class Comment extends Sequelize.Model {
       // id(기본키의 고유 번호), commenter(글쓴이), comment(글 내용), created_at(작성날짜)
       // id는 별도의언급이 없으면 증가되는 고유숫자로 구성된 필드로 자동생성 
 
-      // commenter : 글쓴사람의 id, users테이블의 id 값과 외래키 관계임 ㅏㄸ로 commenter 필드를 만들고 외래키 설정을 하는게 아니라,
+      // commenter : 글쓴사람의 id, users테이블의 id 값과 외래키 관계임 따로 commenter 필드를 만들고 외래키 설정을 하는게 아니라,
       // 외래키 설정하면 자동으로 필드가 삽입됨
       comment: {
         type: Sequelize.STRING(100),
--- Server12/public/sequelize.js
+++ Server12/public/sequelize.js
@@ -1,3 +1,9 @@
+getUser();
+
+
+
+
+
 document.getElementById('user-form').addEventListener('submit',async(e)=>{
       e.preventDefault();
       const name = e.target.username.value;
@@ -7,10 +13,51 @@ document.getElementById('user-form').addEventListener('submit',async(e)=>{
       if(!age){return alert('나이를 입력하세요')};
           
       try{
-           await axios.post('/users/insertuser',{name, age, married});
-      }catch(e){}
+           await axios.post('/users/insert',{name, age, married});
+      }catch(e){
+        console.log(e);
+        getUsers();
+      }
       e.target.username.value = '';
       e.target.age.value = '';
       e.target.married.checked = '';
-
 });     
+
+async function getUser(){
+    try{
+        // 모든 user를 조회새서 user-list를 테이블에 표시함.
+        const result =  await axios.get('/users');
+        const users = result.data;
+
+        const tbody = document.querySelector("#user-list tbody");
+        tbody.innerHTML='';
+    
+        users.map(
+             (user)=>{
+                const row = document.createElement('tr'); // tr 태그 생성
+
+                let td = document.createElement('td');  // td 태그 생성
+                td.textContent = user.id;  // 생성된 태그에 user의 id 삽입
+                row.appendChild(td); // tr 안에 td 삽입
+
+                td = document.createElement('td');  
+                td.textContent = user.name;  
+                row.appendChild(td); 
+
+                td = document.createElement('td');  
+                td.textContent = user.age;  
+                row.appendChild(td); 
+
+                td = document.createElement('td');  
+                td.textContent = user.married ? '기혼' : '미혼0';  
+                row.appendChild(td); 
+
+                tbody.appendChild(row); // 완성된 tr을 tbody에 추가
+        }
+   );
+
+
+    }catch(err){
+        console.log(err);
+    }
+};
