4e4d84d5761f1dd0c4b86e57b283283c3dc0161d
parkminseok56 <133790403+parkminseok56@users.noreply.github.com>
2023-07-26 15:21:02 +0900

    2023-07-26 5교시

--- 04_Express/Server14/routers/page.js
+++ 04_Express/Server14/routers/page.js
@@ -5,4 +5,8 @@ router.get('/',(req,res,next)=>{
       res.render('main',{ title:'NodeGram'});
 });
 
+router.get('/join', (req,res,next)=>{
+    res.render('join',{ title:'회원가입 - NodeGram'});
+});
+
 module.exports = router;
